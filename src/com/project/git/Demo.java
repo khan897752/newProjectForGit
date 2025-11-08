f9782c91fdb4e068cd50d18c2dd7afb5d595e237
pathan farhad <pfarhad315@gmail.com>
2025-11-08 08:05:40 +0530

    Second Commit

--- src/com/project/git/Demo.java
+++ src/com/project/git/Demo.java
@@ -2,7 +2,7 @@ package com.project.git;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Hello Java good morning,hii");
+		System.out.println("Hello Java good morning");
 		
 	}
 
